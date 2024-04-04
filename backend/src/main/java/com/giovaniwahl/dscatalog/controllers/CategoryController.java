a1d0ffb31fa58371737685ad83adbfcef8094bad
giovani wahl <giovaniwahl@gmail.com>
2024-04-04 13:22:17 -0300

    insert new category

--- backend/src/main/java/com/giovaniwahl/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/giovaniwahl/dscatalog/controllers/CategoryController.java
@@ -5,10 +5,7 @@ import com.giovaniwahl.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,9 @@ public class CategoryController {
     @GetMapping("/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.insert(dto));
     }
 }
--- backend/src/main/java/com/giovaniwahl/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/giovaniwahl/dscatalog/services/CategoryService.java
@@ -25,4 +25,12 @@ public class CategoryService {
        Category category = repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Id Not Found."));
        return new CategoryDTO(category);
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto){
+        Category category = new Category();
+        category.setName(dto.getName());
+       category = repository.save(category);
+        return new CategoryDTO(category);
+    }
 }
