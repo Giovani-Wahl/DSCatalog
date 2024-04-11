2d210d44e8814a63f95d7d749d9d0a63c737d6d0
giovani wahl <giovaniwahl@gmail.com>
2024-04-11 15:17:52 -0300

    tests com JUnit

---
+++ backend/src/test/java/com/giovaniwahl/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,30 @@
+package com.giovaniwahl.dscatalog.repositories;
+
+import com.giovaniwahl.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+    @Autowired
+    private ProductRepository repository;
+
+    private Long existingId;
+    @BeforeEach
+    void setUp()throws Exception{
+        existingId = 1L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists(){
+        repository.deleteById(existingId);
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+}
